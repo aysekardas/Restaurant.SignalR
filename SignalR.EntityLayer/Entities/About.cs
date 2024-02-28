ecb0ccda7cafc9507137bfb2ac41e3cd6c01fd02
aysekardas <aysekardas7@gmail.com>
2024-02-28 21:54:53 +0300

    Add project files.

---
+++ SignalR.EntityLayer/Entities/About.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public string İmageUrl { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
