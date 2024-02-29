48b594604296218906c4bb1b6339404838638a69
aysekardas <aysekardas7@gmail.com>
2024-02-29 22:23:16 +0300

    update correction

--- SignalR.BusinessLayer/Abstract/IGenericService.cs
+++ SignalR.BusinessLayer/Abstract/IGenericService.cs
@@ -9,7 +9,7 @@ namespace SignalR.BusinessLayer.Abstract
     public interface IGenericService<T> where T : class
     {
         void TAdd(T entity);
-        void TDelet(T entity);
+        void TDelete(T entity);
         void TUpdate(T entity);
         T TGetById(int id);
         List<T> TGetListAll();
--- SignalR.DataAccessLayer/Abstract/IGenericDal.cs
+++ SignalR.DataAccessLayer/Abstract/IGenericDal.cs
@@ -9,7 +9,7 @@ namespace SignalR.DataAccessLayer.Abstract
     public interface IGenericDal<T> where T : class
     {
         void Add(T entity);
-        void Delet(T entity);
+        void Delete(T entity);
         void Update(T entity);
         T GetById(int id);
         List<T> GetListAll();
