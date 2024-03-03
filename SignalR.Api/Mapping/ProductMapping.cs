c730b2cd4288cbdfe83d1f4d079d36809dd90d79
aysekardas <aysekardas7@gmail.com>
2024-03-03 23:52:12 +0300

    add entities mapping, automapper

---
+++ SignalR.Api/Mapping/ProductMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.ProductDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.Api.Mapping
+{
+    public class ProductMapping : Profile
+    {
+        protected ProductMapping()
+        {
+            CreateMap<Product, GetProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+        }
+    }
+}
